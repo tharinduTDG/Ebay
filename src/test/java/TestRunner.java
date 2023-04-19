4e49ec3839d630450c08047b0c3ad73efabc3826
tharindu <tharindu@globalwavenet.com>
2023-04-20 00:32:07 +0530

    initial commit

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,13 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features ={"src/test/java/Ebay.feature"},
+        glue = {""}
+
+
+)
+public class TestRunner {
+}
